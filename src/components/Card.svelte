dc102828976e4f7c095ab365d3f17300201a542c
Gareeb Vi <61916927+kai00588@users.noreply.github.com>
2024-01-17 17:55:12 +0000

    commit then

---
+++ src/components/Card.svelte
@@ -0,0 +1,7 @@
+<script>
+  export let data;
+</script>
+<Card padding="none">
+    <a href="/animes">
+    </a>
+</Card>
--- src/components/RecentEpisodes.svelte
+++ src/components/RecentEpisodes.svelte
@@ -1,11 +1,12 @@
 <script>
 export let animes;
+import Card from '../components/Card.svelte';
 </script>
 
 <section class="text-center">
   <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 max-w-6xl mx-auto">
     {#each animes as anime}
-        <h2>{anime.title}</h2>
+        <Card anime={anime}/>
     {/each}
   </div>
 </section>
