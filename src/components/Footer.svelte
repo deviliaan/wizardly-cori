119ebb7d2674da352726cb1b842d726c0af76e2a
CodeSandbox <support@codesandbox.io>
2024-01-17 15:56:08 +0000

    Initial commit

---
+++ src/components/Footer.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { Footer, FooterBrand, FooterLink, FooterLinkGroup, FooterCopyright } from 'flowbite-svelte';
+</script>
+
+<Footer footerType="logo">
+    <div class="sm:flex sm:items-center sm:justify-between">
+        <FooterBrand href="https://flowbite.com" src="/images/flowbite-svelte-icon-logo.svg" alt="Flowbite"/>
+        <FooterLinkGroup ulClass="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0">
+            <FooterLink>About</FooterLink>
+            <FooterLink>Privacy Policy</FooterLink>
+            <FooterLink>Blog</FooterLink>
+        </FooterLinkGroup>
+    </div>
+    <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8"/>
+    <FooterCopyright by="Flowbite" year={2024} />
+</Footer>
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,27 @@
+<script>
+    import {Navbar,NavBrand,NavHamburger,NavUl,NavLi,Button,Input} from 'flowbite-svelte';
+    import {SearchOutline} from 'flowbite-svelte-icons';
+</script>
+<Navbar>
+    <NavBrand>
+        <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+        <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+    </NavBrand>
+    <div class="flex md:order-2">
+        <Button color="none" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1">
+            <SearchOutline class="w-5 h-5"/>
+        </Button>
+        <div class="relative hidden md:block">
+            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <SearchOutline class="w-5 h-5 text-gray-500 dark:text-gray-400"/>
+            </div>
+            <Input placeholder="Search" class="pl-10"/>
+        </div>
+    </div>
+    <NavHamburger />
+    <NavUl>
+        <NavLi href="/" active={true}>Popular</NavLi>
+        <NavLi href="/about">Top Airing</NavLi>
+        <NavLi href="/services">Categories</NavLi>
+    </NavUl>   
+</Navbar>
